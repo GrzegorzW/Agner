3fca18e1722c321122dc0518bdab8d5dd5577d98
GrzegorzW <grzegorz.waszkowiak@gmail.com>
2017-10-22 22:22:14 +0200

    Supervisor for chat and player server

--- src/agner_sup.erl
+++ src/agner_sup.erl
@@ -5,8 +5,16 @@
 -export([init/1]).
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+  ChildSpecs = [{
+    slack_chat,
+    {agner_slack_chat, start_link, []},
+    permanent, 5000, worker, [agner_slack_chat]
+  }, {
+    player_server,
+    {agner_player_server, start_link, []},
+    permanent, 5000, worker, [agner_player_server]
+  }],
+  {ok, {{one_for_one, 5, 60}, ChildSpecs}}.
