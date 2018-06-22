536b6c9d3facd3a99a7c31b6dbefc505be8f55db
g.waszkowiak <grzegorz.waszkowiak@gmail.com>
2018-06-22 08:00:40 +0200

    Add configurable port

--- src/agner_app.erl
+++ src/agner_app.erl
@@ -6,6 +6,7 @@
 
 start(_Type, _Args) ->
   ok = error_logger:logfile({open, "agner.log"}),
+  {ok, Port} = application:get_env(port),
   Dispatch = cowboy_router:compile([
     {'_', [
       {"/", cowboy_static, {priv_file, agner, "index.html"}},
@@ -13,7 +14,7 @@ start(_Type, _Args) ->
       {"/player", agner_player_client, []}
     ]}
   ]),
-  {ok, _} = cowboy:start_clear(http, [{port, 7777}], #{env => #{dispatch => Dispatch}}),
+  {ok, _} = cowboy:start_clear(http, [{port, Port}], #{env => #{dispatch => Dispatch}}),
 
   agner_sup:start_link().
 
